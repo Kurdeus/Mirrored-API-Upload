343486ff6b3495bf9ebe04441bd71444cb5982e1
Kurdeus <111803918+Kurdeus@users.noreply.github.com>
2022-10-09 22:23:00 +0330

    Update remote_upload.py

--- remote_upload.py
+++ remote_upload.py
@@ -3,7 +3,7 @@ import requests
 
 
 
-class Mirrpred_Upload:
+class Mirrored_Upload:
 	def __init__(self, api_key):
 		self.api_key = api_key
 		self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',}
